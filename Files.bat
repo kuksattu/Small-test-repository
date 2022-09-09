995d406c2c5ae20783d48397a59b2bfd1c0280e6
kuksa <kuksattu@gmail.com>
2022-09-09 22:12:46 +0300

    Finished the test.

--- Files.bat
+++ Files.bat
@@ -1,2 +1,10 @@
-cd .\_patches-dwnld
-py -3 "..\..\..\bin\scripts\repo_update.py"
+REM Totally didn't copy this from zero318
+setlocal EnableDelayedExpansion
+for /f "tokens=*" %%g in (.\_sharedpatches.txt) do (
+    echo ".\%%g" ".\_patches-dwnld\%%g"
+    xcopy ".\%%g" ".\_patches-dwnld\%%g" /e /d /i /y /q
+)
+@REM pushd .\_patches-dwnld
+@REM py -3 "..\..\..\bin\scripts\repo_update.py"
+@REM popd
+pause
