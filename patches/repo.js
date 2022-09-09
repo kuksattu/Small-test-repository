3e8c16b6808a2367dc7e28410fbf82671add9915
kuksa <kuksattu@gmail.com>
2022-09-09 19:46:04 +0300

    Initial commit

---
+++ patches/repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "",
+	"id": "testu",
+	"patches": {
+		"Marker": "A small patch to see if a patch directory works."
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/kuksattu/kuksa/master/"
+	],
+	"title": "Kuksa's test repository"
+}
